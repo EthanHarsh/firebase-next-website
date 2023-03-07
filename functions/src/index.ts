7cc65a14165f2477dc297b75245da09d606080d7
Ethan Harsh <ethan@ethanharsh.com>
2023-03-06 23:36:43 -0500

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,8 @@
 import * as functions from "firebase-functions";
 import {checkRssFeed} from "./medium";
 import {checkFeaturedRepos} from "./github";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
 exports.checkRssFeed = functions.https.onCall(checkRssFeed);
 exports.checkFeaturedRepos = functions.https.onCall(checkFeaturedRepos);
