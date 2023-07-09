a3cc74f7fedd3359999bad7d51f76ea6b91748e2
Ethan Harsh <ethan@ethanharsh.com>
2023-07-09 00:14:16 -0400

    remove old frontend

--- ionic_frontend/src/constants/breakpoints.ts
+++
@@ -1,1 +0,0 @@
-export const cardBreakpoint = 992;
--- ionic_frontend/src/store.ts
+++
@@ -1,9 +0,0 @@
-import { reactive } from "vue";
-import { cardBreakpoint } from "./constants/breakpoints";
-
-export const store = reactive({
-  mobileSize: window.innerWidth < cardBreakpoint,
-  updateMobileSize() {
-    this.mobileSize = window.innerWidth < cardBreakpoint;
-  },
-});
